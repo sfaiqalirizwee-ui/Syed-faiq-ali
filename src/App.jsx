99b0b35697b1655dfe9fe56d320c886bdec68bde
syedfaiqali <sfaiqalirozwee@gmail.com>
2026-07-11 06:26:21 -0700

    Add Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+import Footer from "./components/footer";
 
 export default function App() {
   return (
